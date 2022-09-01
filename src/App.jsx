1b811aaa3d9aaac70b179d03f6db857f8382818a
Seb Arz <109311254+websebarz@users.noreply.github.com>
2022-09-01 10:42:58 -0500

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import Counter from "./comps/Counter";
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Base Apparel coming soon...</h1>
+      <Counter />
     </>
   );
 }
