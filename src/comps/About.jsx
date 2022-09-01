b1ecc530340262cf55e5441d1a6ac76fd36f1f56
Seb Arz <109311254+websebarz@users.noreply.github.com>
2022-09-01 13:25:42 -0500

    Added form

--- src/comps/About.jsx
+++ src/comps/About.jsx
@@ -9,6 +9,27 @@ export default function About() {
         Add your email below to stay up-to-date with announcements and our
         launch deals.
       </p>
+      <form className="mt-8">
+        <input
+          type="text"
+          name="name"
+          placeholder="Email Address"
+          className="relative w-full h-[55px] py-3 px-6 rounded-full"
+        />
+        <button
+          type="submit"
+          className="absolute w-[70px] h-[55px] bottom-12 right-4 bg-secondary rounded-full p-3 flex justify-center items-center shadow-lg"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20">
+            <path
+              fill="none"
+              stroke="#FFF"
+              stroke-width="2"
+              d="M1 1l8.836 8.836L1 18.671"
+            />
+          </svg>
+        </button>
+      </form>
     </div>
   );
 }
