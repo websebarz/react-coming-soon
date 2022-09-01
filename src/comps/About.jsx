ba6e8f81d775fa78d019181cb5e910deb973abd8
Seb Arz <109311254+websebarz@users.noreply.github.com>
2022-09-01 13:59:29 -0500

    Fixed btn position issue

--- src/comps/About.jsx
+++ src/comps/About.jsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="p-4 mt-16 text-center">
+    <div className="relative max-w-sm p-4 mt-16 text-center mr-auto ml-auto">
       <h1 className="text-5xl tracking-widest">
         <span className="font-light text-primary">WE'RE</span> COMING SOON
       </h1>
@@ -14,11 +14,12 @@ export default function About() {
           type="text"
           name="name"
           placeholder="Email Address"
-          className="relative w-full h-[55px] py-3 px-6 rounded-full"
+          className="w-full p-4 px-6 rounded-full"
         />
         <button
           type="submit"
-          className="absolute w-[70px] h-[55px] bottom-12 right-4 bg-secondary rounded-full p-3 flex justify-center items-center shadow-lg"
+          className="absolute w-[80px] h-[3.725em] right-4 bottom-4 bg-secondary rounded-full
+           flex justify-center items-center shadow-lg"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20">
             <path
